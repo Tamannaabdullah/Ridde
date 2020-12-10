4722a640eb1ab8c30b74878bf0b560fb5e192332
Dysiewicz <joshua.dysiewicz@fujitsu.com>
2020-12-10 14:53:25 +0000

    removed sql stuff

--- sql/postgresConfig.js
+++ sql/postgresConfig.js
@@ -4,18 +4,18 @@ const pool = new Pool({
     connectionString: "postgres://wcgmyneu:CBH50DEPRmfV_IFsVpz54rTWJtO1Bvth@kandula.db.elephantsql.com:5432/wcgmyneu"
 });
 
-const test = async () => {
-    await pool.query(`INSERT INTO users(name, score) VALUES('leyla', 0)`);
-    await pool.query(`INSERT INTO users(name, score) VALUES('josh', 0)`);
-    await pool.query(`INSERT INTO users(name, score) VALUES('olivia', 0)`);
-    await pool.query(`INSERT INTO users(name, score) VALUES('jakewych', 0)`);
-    await pool.query(`INSERT INTO users(name, score) VALUES('jakewilkinson', 0)`);
-    await pool.query(`INSERT INTO users(name, score) VALUES('harrison', 0)`);
-    await pool.query(`INSERT INTO users(name, score) VALUES('leon', 0)`);
-    await pool.query(`INSERT INTO users(name, score) VALUES('olivia', 0)`);
-}
+// const test = async () => {
+//     await pool.query(`INSERT INTO users(name, score) VALUES('leyla', 0)`);
+//     await pool.query(`INSERT INTO users(name, score) VALUES('josh', 0)`);
+//     await pool.query(`INSERT INTO users(name, score) VALUES('olivia', 0)`);
+//     await pool.query(`INSERT INTO users(name, score) VALUES('jakewych', 0)`);
+//     await pool.query(`INSERT INTO users(name, score) VALUES('jakewilkinson', 0)`);
+//     await pool.query(`INSERT INTO users(name, score) VALUES('harrison', 0)`);
+//     await pool.query(`INSERT INTO users(name, score) VALUES('leon', 0)`);
+//     await pool.query(`INSERT INTO users(name, score) VALUES('olivia', 0)`);
+// }
 
-test();
+// test();
 
 module.exports = pool;
 
